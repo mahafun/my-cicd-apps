af857aa12a24e91ea15829afceb88f00f939d681
Danai S <thaiwebapps@gmail.com>
2025-02-13 14:08:27 +0700

    Add MySQL connection setup and configuration files

---
+++ mysql-app/app.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const dbConfig = require('./dbConfig');
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as id', connection.threadId);
+
+  // Perform a simple query
+  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+    if (error) throw error;
+    console.log('The solution is:', results[0].solution);
+  });
+
+  connection.end();
+});
