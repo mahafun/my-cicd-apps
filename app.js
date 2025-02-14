011548826043882d6e9ff9b6ccaf2ab8a1fc1981
Danai S <thaiwebapps@gmail.com>
2025-02-14 17:58:07 +0700

    Refactor database connection to use mysql2 with async/await

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const dbConfig = require('./dbConfig');
 
-const connection = mysql.createConnection(dbConfig);
+async function main() {
+  try {
+    console.log('Connecting to the database...');
+    const connection = await mysql.createConnection(dbConfig);
+    console.log('Connected to the database as id', connection.threadId);
 
-console.log('Connecting to the database...');
-
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
-  }
-  console.log('Connected to the database as id', connection.threadId);
-
-  // Perform a simple query
-  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-    if (error) throw error;
+    // Perform a simple query
+    const [results, fields] = await connection.query('SELECT 1 + 1 AS solution');
     console.log('The solution is:', results[0].solution);
-  });
 
-  connection.end();
-});
+    await connection.end();
+  } catch (err) {
+    console.error('Error connecting to the database:', err.stack);
+  }
+}
+
+main();
