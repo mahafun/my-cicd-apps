88f5fb21c78d3e77b7e60ddb4e630a0752902c7d
Danai S <thaiwebapps@gmail.com>
2025-02-18 16:45:53 +0700

    feat: add KngHome page and integrate React Router for navigation

---
+++ src/pages/KngHome.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const KngHome = () => {
+  return (
+    <div>
+      <h1>Welcome to Kng Home</h1>
+      <p>This is the Kng Home page.</p>
+    </div>
+  );
+};
+
+export default KngHome;
