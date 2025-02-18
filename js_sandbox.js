16c8d79761a7fea843a4268f7c3cde24c7094511
ninerxsolution <45664847+ninerxsolution@users.noreply.github.com>
2025-02-18 12:03:25 +0700

    created minus integer function

--- js_sandbox.js
+++ js_sandbox.js
@@ -4,5 +4,10 @@ function sumIntegers(a, b) {
     return a + b;
 }
 
+function minusIntegers(a, b) {
+    return a - b;
+}
+
 console.log(sumIntegers(1, 2)); // 3
+console.log(minusIntegers(1, 2)); // -1
 console.log(string); // Hello, World!
