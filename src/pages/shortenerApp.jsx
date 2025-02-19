b6ea2cad0685238d65cac80c703ffa5b41b76a3d
ninerxsolution <45664847+ninerxsolution@users.noreply.github.com>
2025-02-19 14:31:33 +0700

    Welcome page for anna home

--- src/pages/shortenerApp.jsx
+++ src/pages/shortenerApp.jsx
@@ -1,3 +1,11 @@
-const advandedOptions = "Advanced Options";
 
-console.log(advandedOptions);
+const anna = () => {
+    return (
+        <div>
+            <h1>Welcome to anna Home</h1>
+            <p>This is the anna Home page.</p>
+        </div>
+    );
+};
+
+export default anna;
